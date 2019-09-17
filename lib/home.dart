0af7ee1d311106f7dac773b245d10d2fddc01054
Vinicius Amélio <vinicius.amelio@zoho.com>
2019-09-17 19:58:08 -0300

    Add dataManipulation functions in home

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,10 +9,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _toDoList = [];
   @override
   Widget build(BuildContext context) {
     return Container(
       
     );
   }
+
+  Future<File> _getFile() async{
+  final dir = await getApplicationDocumentsDirectory(); // Get the directory where i can place files without permissions and this things
+  return File("${dir.path}/data.json");
+  }
+
+  Future<File> _saveData() async{
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async{
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
 }
+
