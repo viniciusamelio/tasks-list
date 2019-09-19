af7522de28fe3e8ac2f946bc9a07c43ea096f767
Vinicius Amélio <vinicius.amelio@zoho.com>
2019-09-19 19:23:57 -0300

    Adding Controller Functions and Design Widgets

--- lib/home.dart
+++ lib/home.dart
@@ -9,12 +9,113 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  @override 
+  void initState(){
+    super.initState();
+
+    _readData().then((data){     
+      setState(() {
+        _toDoList = jsonDecode(data);
+      });
+    });
+  }
+
   List _toDoList = [];
+
+  final _toDoController = TextEditingController();
+
+  void addToDo(){
+    setState(() {
+      Map<String,dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"]=false;
+      _toDoList.add(newToDo); 
+      _saveData();
+    });
+  }
+
+  Widget BuildTile(context,index){
+        return Dismissible(
+          key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+          background: Container(
+            color: Colors.red,
+            child: Align(
+              alignment: Alignment(-0.9,0),
+              child: Icon(Icons.delete,color: Colors.white,),
+            ),
+          ),
+          direction: DismissDirection.startToEnd,
+          child: CheckboxListTile(
+                          title: Text(_toDoList[index]["title"],style: TextStyle(color: Colors.black),),
+                          value: _toDoList[index]["ok"],
+                          secondary: CircleAvatar(child: Icon(_toDoList[index]["ok"]?
+                            Icons.check : Icons.error
+                          ),),
+                          onChanged: (value){
+                            setState(() {
+                              _toDoList[index]["ok"] = value;
+                              _saveData();
+                            });
+                          },
+                        ),
+        ); 
+   } 
+
+                      
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.indigoAccent,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    child:TextFormField(
+                      controller: _toDoController,
+                      decoration: InputDecoration(
+                      labelText: "Nome da tarefa",
+                      labelStyle: TextStyle(color:Colors.indigoAccent
+                      ),
+                    )
+                  )
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top:12),
+                    child: RaisedButton(
+                    onPressed:()=>addToDo(),
+                    color: Colors.indigoAccent,      
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),                                                    
+                    child: Text("Adicionar",style: TextStyle(color: Colors.white),)
+                  ),
+                  ),
+                ],
+            ),            
+            ),
+            Container(
+              height: 550 ,
+                    child: ListView.builder(
+                      padding: EdgeInsets.only(top:10),
+                      itemCount: _toDoList.length,
+                      itemBuilder: BuildTile,
+                    ),
+                  ),                  
+            
+          ],
+        ),
+      ),
+    );  
+     
   }
 
   Future<File> _getFile() async{
